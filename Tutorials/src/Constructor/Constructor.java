47d1b95c8381e1a53a514407bc331f70ad1bcbcb
Rahul K <irahul@live.com>
2021-04-22 15:26:21 +0530

    First commit Constructor

---
+++ Tutorials/src/Constructor/Constructor.java
@@ -0,0 +1,21 @@
+package Constructor;
+
+public class Constructor {
+	int num1;
+	int num1;
+	public Constructor() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Constructor obj = new Constructor();
+		
+	System.out.print(obj.num1);
+		
+		
+
+	}
+
+}
