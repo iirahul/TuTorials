bad35a8e9a173704b0efd29c162da2033af8f777
Rahul K <irahul@live.com>
2021-04-22 18:16:13 +0530

    First commit of Static demo

--- Tutorials/src/ConstructorOverloading/CoverLo.java
+++ Tutorials/src/ConstructorOverloading/CoverLo.java
@@ -1,8 +1,19 @@
 package ConstructorOverloading;
 
 public class CoverLo {
+	
+	
+		int num1;
+		int num2;
+		public CoverLo() {
+			
+		}
+		
+        public CoverLo(int veriable) {
+			
+		}
 
-	public static void main(String[] args) {
+	    public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
---
+++ Tutorials/src/StaticDemo/StaticDemo.java
@@ -0,0 +1,37 @@
+package StaticDemo;
+
+public class StaticDemo {
+	
+	int salary;
+	int eid ;
+	static String ceo;
+	public StaticDemo() {
+		salary=5000;
+		eid=1;
+		ceo = "pankaj";
+	}
+	
+	public void show() {
+		System.out.println(eid+" ; " +salary+" ; " +ceo);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StaticDemo rahul = new StaticDemo();
+		StaticDemo naveen = new StaticDemo();
+		
+		
+		rahul.eid = 2233;
+		rahul.salary = 100000;
+		StaticDemo.ceo = "Harish";
+		
+		rahul.show();
+		naveen.show();
+		
+		
+		
+
+	}
+
+}
